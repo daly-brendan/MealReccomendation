9eb531957ef067119a21b20b331797aa33da54ec
daly-brendan <dalysbrendan@gmail.com>
2021-09-24 17:59:11 -0400

    Using Spyder
    from now on - VSC too buggy and unresponsive in installing packages

--- New Direction_Spyder.py
+++ New Direction_Spyder.py
@@ -1,5 +1,10 @@
-meals = ["Aglio e Olio", "Cacio e Pepe", "Bouef Bourginon"]
 
+import os
+import numpy as np
+import pandas as pd
+import matplotlib
+
+meals = ["Aglio e Olio", "Cacio e Pepe", "Bouef Bourginon"]
 
 wines = ['Washington Pinot Noir', 'Gamay', 'Napa Cabernet Sauv', 'Alexander Valley Cabernet Sauv', 'Malbec', 'Cabernet Franc', 'Valpolicella', 'Sancerre', 'Sauvignon Blanc', 'Chablis', 'Chardonnay', 'Bourginon Blanc' 'Albarino', 'Champagne', 'Vermentina', 'Gavi', 'Super Tuscan', 'Riesling']
 print (wines)
